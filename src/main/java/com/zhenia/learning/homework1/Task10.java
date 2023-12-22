9b4f39d8abab66869ed03e9a9f157817f12c0ab6
zhenia0908 <eugesha0908@gmail.com>
2023-12-22 15:04:46 +0100

    Homework task10

--- src/main/java/com/zhenia/learning/homework1/Task10.java
+++ src/main/java/com/zhenia/learning/homework1/Task10.java
@@ -1,10 +1,29 @@
 package com.zhenia.learning.homework1;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+/*	Напишите программу для заполнения массива случайными числами
+        от 1 до 100 до тех пор, пока сумма элементов массива не превысит значение 1000.
+        Используйте цикл do-while для выполнения этой задачи.*/
 public class Task10 {
 
     public static void main(String[] args) {
-     //int array [] = new int[];
-        int b = 10;
+        Random random = new Random();
+        int sum = 0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        do { int i = 0;
+           // for (int i = 0; i < numbers.size(); i++) { // метод getLast возвращает последний элемент
+               numbers.add(random.nextInt(100) + 1);
+                sum += numbers.get(i);
+                i++;
+
+        } while (sum <= 1000);
+        System.out.println("Summa: " + sum);
 
     }
+
+
 }
+
