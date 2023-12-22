a5e4c097814d81b68f8963b2f2d05d1932bf24ea
zhenia0908 <eugesha0908@gmail.com>
2023-12-22 15:17:13 +0100

    Homework task11

--- src/main/java/com/zhenia/learning/homework1/Task11.java
+++ src/main/java/com/zhenia/learning/homework1/Task11.java
@@ -1,8 +1,38 @@
 package com.zhenia.learning.homework1;
 
+import java.util.Scanner;
+
 public class Task11 {
+/*	Напишите программу, которая находит первое простое число в диапазоне от 1 до 100.
+ Простое число - это число, которое делится только на себя и на 1.
+ */
+
 
     public static void main(String[] args) {
 
+        for (int i = 2; i <= 100; i++) {
+            switch (i) {
+                case 2:
+                    System.out.println("Number is: " + i);
+                    break;
+                case 3:
+                    System.out.println("Number is: " + i);
+                    break;
+                case 5:
+                    System.out.println("Number is: " + i);
+                    break;
+                case 7:
+                    System.out.println("Number is: " + i);
+                    break;
+
+                default:
+                    if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+
+                        System.out.println("Number is: " + i);
+                        break;
+                    }
+            }
+            break;
+        }
     }
 }
